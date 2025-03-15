3cb2407c4c338da09ea141d9c9386471f9fe118e
Adarsh Roy <adarshroy2010@gmail.com>
2025-03-15 21:54:08 +0530

    Hero Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
+import HeroSection from "@/components/HeroSection";
+
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[.96] antialiased bg-grid-white/[.02] ">
-        <h1 className="bg-pink-500 text-yellow-500 text-xl text-center ">
-          Hello hi 
-        </h1>
+          <HeroSection/>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,23 @@
 import type { Config } from "tailwindcss";
 
+// const svgToDataUri = require("mini-svg-data-uri");
+// const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
+function addVariablesForColors({ addBase, theme }: any) {
+  const allColors = flattenColorPalette(theme('colors'));
+  const newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, value]) => [`--${key}`, value])
+  );
+
+  addBase({
+    ':root': newVars,
+  });
+}
+
+
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,11 +27,26 @@ export default {
   darkMode: 'class',
   theme: {
     extend: {
+      animation: {
+        spotlight: "spotlight 2s ease .75s 1 forwards",
+      },
+      keyframes: {
+        spotlight: {
+          "0%": {
+            opacity: '0',
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: '1',
+            transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
     },
   },
-  plugins: [],
+  plugins: [addVariablesForColors],
 } satisfies Config;
