f07c0258f165d1a0c3ff9c7777f7d6a3611d169f
Adarsh Roy <adarshroy2010@gmail.com>
2025-03-17 13:39:58 +0530

    all courses

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images:{
-    domains:['images.unsplash.com','res.cloudinary.com']
+    domains:['images.unsplash.com','res.cloudinary.com','encrypted-tbn0.gstatic.com','static.toiimg.com','www.billboard.com'
+      ,'variety.com'
+    ]
   }
 };
 
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,16 @@
+'use client'
+import Image from "next/image";
+import React from "react";
+import { CardBody, CardContainer, CardItem } from "@/components/ui/3d-card";
+import courseData from "@/data/music_courses.json"
+
+function page() {
+  return (
+    <div className="min-h-screen bg-black py-12 pt-36">
+        <h1 className="text-lg md:text-7xl text-center font-sans font-bold mb-8 text-white">All courses ({courseData.courses.length})</h1>  
+        
+    </div>
+  )
+}
+
+export default page
