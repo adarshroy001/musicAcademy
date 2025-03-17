04560001a3305ac31144df21a1662b21e6fd629e
Adarsh Roy <adarshroy2010@gmail.com>
2025-03-17 10:31:56 +0530

    Featered Courses

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[.96] antialiased bg-grid-white/[.02] ">
           <HeroSection/>
+          <FeaturedCourses/>
     </main>
   );
 }
